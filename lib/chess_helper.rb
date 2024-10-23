f97dced5d9044e3d091734fb43e9895e33f5dc74
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2024-10-23 17:00:09 +0200

    write and test index_to_coord method on Board

--- lib/chess_helper.rb
+++ lib/chess_helper.rb
@@ -10,6 +10,18 @@ module ChessHelper
 		"h" => 7
  	}
 
+ 	INDEXES_MAPPING = {
+    0 => "a",
+    1 => "b",
+    2 => "c",
+    3 => "d",
+    4 => "e",
+    5 => "f",
+    6 => "g",
+    7 => "h"
+	}
+
+
  	def coord_to_index(x, y)
 		return nil unless MAPPING.keys.include?(x) && (1..8).include?(y)
 
@@ -22,6 +34,12 @@ module ChessHelper
 	def index_to_coord(index)
 		return nil unless (0..63).include?(index)
 
-		
+		#find row (letter from a to h)
+		row = INDEXES_MAPPING[index % 8]
+
+		#find column (number from 8 to 1)
+		col = 8 - (index / 8)
+
+		[row, col]
 	end
 end
