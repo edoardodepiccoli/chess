9592a6dae680cff102495ee157a65d70c4dd7b89
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2024-10-27 21:32:51 +0100

    move printing in board printer, add position class

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 require "colorize"
 
 require_relative "chess_helper"
+require_relative "position"
+require_relative "board_printer"
 require_relative "player"
 
 require_relative "pieces/piece"
@@ -14,6 +16,7 @@ require_relative "pieces/king"
 
 class Board
 	include ChessHelper
+	include BoardPrinter
 
 	attr_reader :board
 
@@ -21,18 +24,6 @@ class Board
 		@board = Array.new(8) { Array.new(8) }
 
 		initialize_board
-	end
-
-	def print_board
-		print "   a  b  c  d  e  f  g  h \n"
-		@board.each_with_index do |row, row_index|
-			print "#{8 - row_index} "
-			row.each_with_index do |col, col_index|
-				print_cell_at(ChessHelper.parse_coordinates([row_index, col_index])) # fix this
-			end
-			print " #{8 - row_index}\n"
-		end
-		print "   a  b  c  d  e  f  g  h \n"
 	end
 
 	def cell_empty?(position)
@@ -63,7 +54,6 @@ class Board
 
 	def checkmate?(player)
 		color = player.color
-		
 		return true unless player_has_available_moves?(player)
 
 		false
@@ -166,26 +156,6 @@ class Board
 
 		true
 	end
-
-	# utilities to print the board
-	def print_cell_at(position)
-		cell_color = (ChessHelper.parse_algebraic(position).sum).even? ? :none : :red
-
-		print_empty_cell(position) if cell_empty?(position)
-		print_piece(position) unless cell_empty?(position)
-	end
-
-	def print_empty_cell(position)
-		cell_color = (position.sum).even? ? :none : :red
-		print "   ".colorize(:background => cell_color)
-	end
-
-	def print_piece(position)
-		cell_color = (position.sum).even? ? :none : :red
-		current_piece = piece_at(position)
-		print " #{current_piece.unicode} ".colorize(current_piece.color).colorize(:background => cell_color)
-	end
-	# end utilities to print the board
 
 	def initialize_board
 		place_new_pieces([
