5765b8f222f135de40fe691313f5d5d7af8faeca
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2024-10-29 14:53:02 +0100

    add move validation for king class

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -5,4 +5,18 @@ class Knight < Piece
     super(color)
     @unicode = '♘'
   end
+
+  def available_positions(current_pos, board)
+    current_row, current_col = current_pos
+
+    steps = [
+      [2, 1], [-2, 1], [2, -1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]
+    ]
+
+    steps
+      .map { |step_row_offset, step_col_offset| [current_row + step_row_offset, current_col + step_col_offset] }
+      .filter { |row, col| row.between?(0, 7) && col.between?(0, 7) }
+      .filter { |pos| board.cell_at(pos).nil? || board.cell_at(pos).color != @color }
+      .sort
+  end
 end
--- spec/pieces/knight_spec.rb
+++ spec/pieces/knight_spec.rb
@@ -3,5 +3,60 @@ require_relative '../../lib/pieces/knight'
 require_relative '../../lib/board'
 
 describe Knight do
+  subject(:knight) { Knight.new(:white) }
 
+  describe '#available_positions' do
+
+    context 'when board just got initialized' do
+      it 'should return two positions' do
+        board = Board.new
+
+        expect(knight.available_positions([7, 1], board)).to eql([[5, 0], [5, 2]].sort)
+      end
+    end
+
+    context 'when it is in the middle of an empty board' do
+      it 'should return eight positions' do
+        board = Board.new
+        board.clear_board
+
+        expect(knight.available_positions([4, 5], board)).to eql([[6, 4], [5, 3], [3, 3], [2, 4], [2, 6], [3, 7], [5, 7], [6, 6]].sort)
+      end
+    end
+
+    context 'when it is in the middle of an empty board and an opponent piece is in eating position' do
+      it 'should return eight positions including the one where the opponent piece is standing' do
+        board = Board.new
+        board.clear_board
+
+        board.place_new_pieces([
+                                 [Pawn, :black, [[6, 4]]]
+                               ])
+
+        expect(knight.available_positions([4, 5], board)).to eql([[6, 4], [5, 3], [3, 3], [2, 4], [2, 6], [3, 7], [5, 7], [6, 6]].sort)
+      end
+    end
+
+    context 'when it is in the middle of an empty board and an ally piece is in eating position' do
+      it 'should return eight positions excluding the one where the opponent piece is standing' do
+        board = Board.new
+        board.clear_board
+
+        board.place_new_pieces([
+                                 [Pawn, :white, [[6, 4]]]
+                               ])
+
+        expect(knight.available_positions([4, 5], board)).to eql([[5, 3], [3, 3], [2, 4], [2, 6], [3, 7], [5, 7], [6, 6]].sort)
+      end
+    end
+
+    context 'when it is at the edge of the board' do
+      it 'should return four positions' do
+        board = Board.new
+        board.clear_board
+
+        expect(knight.available_positions([3, 0], board)).to eql([[1, 1], [2, 2], [4, 2], [5, 1]].sort)
+      end
+    end
+  end
 end
